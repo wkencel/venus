88f2266a0fdf44dcf8ff58f1e938890e152079de
Will Kencel <wkencel1@gmail.com>
2021-01-20 03:51:36 -0500

    cleaned up chart container

--- src/containers/ChartContainer.tsx
+++ src/containers/ChartContainer.tsx
@@ -4,9 +4,9 @@
  */
 
 import React from "react";
-import { LineGraph } from "../charts/LineGraph"
-import { AreaChart } from "../charts/AreaChart"
-import { CardDropDown } from "../components/CardDropDown";
+// import { LineGraph } from "../charts/LineGraph"
+// import { AreaChart } from "../charts/AreaChart"
+// import { CardDropDown } from "../components/CardDropDown";
 // import { TestChart } from "../charts/TestChart";
 import PieChart from "../charts/PieChart";
 // import { Chart } from "../components/Chart";
@@ -25,26 +25,14 @@ function ChartContainer(): JSX.Element {
    
     <div id="chartContainer">
         <Row gutter={[16, 16]}>
-          <Col span={12}>
-            <div className="graphHolder">
-              <LineGraph />
-            </div>
-          </Col>
-          <Col span={12}>
-            <div className="graphHolder" >
-              <AreaChart />
-            </div>
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]}>
         <Col span={12}>
             <div className="graphHolder">
-              <LineGraph />
+			<PieChartRender />
             </div>
           </Col>
-          <Col span={12}>
+          <Col span={2}>
             <div className="graphHolder">
-			  <PieChartRender />
+			  <ChartRender />
             </div>
           </Col>
         </Row>
